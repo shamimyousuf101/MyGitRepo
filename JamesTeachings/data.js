4edfd186466b86897d352869fbea482509729221
ShamimYousuf <waheedayousuf20@gmail.com>
2018-08-21 23:38:45 +0100

    review comments incorporated

---
+++ JamesTeachings/data.js
@@ -0,0 +1,7 @@
+var theme = {
+	LimeAndGreen :"#E7DAA1,#AAA739,#6D4600",
+	PinkAndViolet :"#B47EA0, #E19DA2,#852C64",
+	SilverAndBlue:"#9FB9F6, #C0C0C0,#6C8196"
+}
+
+var images = ['images1.jpg', 'images2.jpg', 'images3.jpg', 'images4.jpg', 'images5.jpg', 'images6.jpg', 'images7.png' ]
