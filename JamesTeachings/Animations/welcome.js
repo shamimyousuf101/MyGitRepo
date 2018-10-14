ef022db8833dd859e1bbe283c944e8594b8f1cc8
ShamimYousuf <waheedayousuf20@gmail.com>
2018-10-14 22:46:04 +0100

    new link to cloud page

--- JamesTeachings/Animations/welcome.js
+++ JamesTeachings/Animations/welcome.js
@@ -1,8 +1,10 @@
-document.addEventListener("dblclick", pause);
+document.addEventListener("click", pause);
 
 function pause(event){
   const element1 = document.querySelector(".line1");
   const element2 = document.querySelector(".line2");
   element1.className = "paused"
   element2.className = "paused"
+  const linktogamesys = document.querySelector(".linktogamesys");
+  linktogamesys.style.display="block";
 }
